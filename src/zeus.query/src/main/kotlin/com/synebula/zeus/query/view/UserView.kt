378f6dcb194b9d9b7b66357ca611c559c098d8ca
alex <reizero@live.com>
2020-05-25 00:09:20 +0800

    增加用户模糊查询功能

--- src/zeus.query/src/main/kotlin/com/synebula/zeus/query/view/UserView.kt
+++ src/zeus.query/src/main/kotlin/com/synebula/zeus/query/view/UserView.kt
@@ -1,5 +1,8 @@
 package com.synebula.zeus.query.view
 
+import com.synebula.gaea.query.Operator
+import com.synebula.gaea.query.Where
+
 class UserView {
     var id: String = ""
 
@@ -7,6 +10,7 @@ class UserView {
 
     var password: String = ""
 
+    @Where(Operator.like)
     var realName: String? = null
 
     var phone: String? = null
